b4f20d22235fb64376d60136993ddab1372fef44
jinxluck <janschiermer@gmail.com>
2019-11-26 18:12:07 +0100

    Added Timestamp

--- database_poc/models.py
+++ database_poc/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class testdatabase(models.Model):
     Data = models.CharField(max_length=100)
+    Timestamp = models.DateTimeField(auto_now='True')
 
     def __str__(self):
         return self.Data
